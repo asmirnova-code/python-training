1d26afe2cd2e952478cb27f53868d3b199cc9363
Anastasia Smirnova <nastja.sm87@gmail.com>
2021-12-25 21:41:44 +0300

    Выделен класс для работы с контактами

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,34 +1,16 @@
-from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-class ContactApplication:
+class ContactHelper:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+    def __init__(self, app):
+        self.app = app
 
     def open_new_contact_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
 
-    def create_new_contact(self, contact):
-        wd = self.wd
+    def create(self, contact):
+        wd = self.app.wd
         self.open_new_contact_page()
         # fill first name
         wd.find_element_by_name("firstname").click()
@@ -103,12 +85,6 @@ class ContactApplication:
         self.return_to_homepage()
 
     def return_to_homepage(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def destroy(self):
-        self.wd.quit()
--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -1,33 +1,33 @@
 import pytest
 from model.contact import Contact
-from fixture.contact_application import ContactApplication
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
-    fixture = ContactApplication()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_create_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="someone", middlename="dita", lastname="loren", nickname="pool",
-                                title="research", company="cuberto", address="St. Petersburg",
-                                home_phone="8974567", mobile="789234567899", work_phone="234567889", fax="76834657",
-                                email="email@somemail.com",
-                                email2="dfghj@mail.com", email3="fghjkk@mail.com", homepage="address.com",
-                                birthday="2", birthmonth="November", birthyear="1970",
-                                address2="one address", home="15", notes="some notes"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="someone", middlename="dita", lastname="loren", nickname="pool",
+                               title="research", company="cuberto", address="St. Petersburg",
+                               home_phone="8974567", mobile="789234567899", work_phone="234567889", fax="76834657",
+                               email="email@somemail.com",
+                               email2="dfghj@mail.com", email3="fghjkk@mail.com", homepage="address.com",
+                               birthday="2", birthmonth="November", birthyear="1970",
+                               address2="one address", home="15", notes="some notes"))
+    app.session.logout()
 
 def test_create_empty_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="",
-                                title="", company="", address="",
-                                home_phone="", mobile="", work_phone="", fax="",
-                                email="",
-                                email2="", email3="", homepage="",
-                                birthday="", birthmonth="-", birthyear="",
-                                address2="", home="", notes=""))
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
+                               title="", company="", address="",
+                               home_phone="", mobile="", work_phone="", fax="",
+                               email="",
+                               email2="", email3="", homepage="",
+                               birthday="", birthmonth="-", birthyear="",
+                               address2="", home="", notes=""))
 
-    app.logout()
+    app.session.logout()
 
