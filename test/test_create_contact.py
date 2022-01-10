94cfea92c8922675d4d2cf846bb9888f6841f84f
Anastasia Smirnova <nastja.sm87@gmail.com>
2022-01-10 15:08:38 +0300

    Реализована валидация фикстуры

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -2,7 +2,6 @@ import pytest
 from model.contact import Contact
 
 def test_create_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="someone", middlename="dita", lastname="loren", nickname="pool",
                                title="research", company="cuberto", address="St. Petersburg",
                                home_phone="8974567", mobile="789234567899", work_phone="234567889", fax="76834657",
@@ -10,10 +9,9 @@ def test_create_contact(app):
                                email2="dfghj@mail.com", email3="fghjkk@mail.com", homepage="address.com",
                                birthday="2", birthmonth="November", birthyear="1970",
                                address2="one address", home="15", notes="some notes"))
-    app.session.logout()
+
 
 def test_create_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                                title="", company="", address="",
                                home_phone="", mobile="", work_phone="", fax="",
@@ -22,5 +20,4 @@ def test_create_empty_contact(app):
                                birthday="", birthmonth="-", birthyear="",
                                address2="", home="", notes=""))
 
-    app.session.logout()
 
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,11 +1,7 @@
 from model.contact import Contact
 
 def test_edit_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="New name"))
-    app.session.logout()
 
 def test_edit_contact_birthday(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(birthday="17"))
-    app.session.logout()
