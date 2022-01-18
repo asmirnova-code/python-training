a3102c693a1ec81aad2f963e65a8cd4f652f13c4
Anastasia Smirnova <nastja.sm87@gmail.com>
2022-01-18 15:18:48 +0300

    Проверки в виде сравнения списков. Проверки добавлены для создания, модификации и удаления контактов.

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -3,29 +3,27 @@ from model.contact import Contact
 
 def test_create_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="someone", middlename="dita", lastname="loren", nickname="pool",
-                               title="research", company="cuberto", address="St. Petersburg",
-                               home_phone="8974567", mobile="789234567899", work_phone="234567889", fax="76834657",
-                               email="email@somemail.com",
-                               email2="dfghj@mail.com", email3="fghjkk@mail.com", homepage="address.com",
-                               birthday="2", birthmonth="November", birthyear="1970",
-                               address2="one address", home="15", notes="some notes")
+    contact = Contact(firstname="some", middlename="dita", lastname="loren", nickname="pool",
+    title="research", company="cuberto", address="St. Petersburg", home_phone="8974567", mobile="789234567899",
+    work_phone="234567889", fax="76834657", email="email@somemail.com", email2="dfghj@mail.com",
+    email3="fghjkk@mail.com", homepage="address.com", birthday="15", birthmonth="November", birthyear="1970",
+    address2="one address", home="15", notes="some notes")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_create_empty_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="someone", middlename="dita", lastname="loren", nickname="pool",
-                      title="research", company="cuberto", address="St. Petersburg",
-                      home_phone="8974567", mobile="789234567899", work_phone="234567889", fax="76834657",
-                      email="email@somemail.com",
-                      email2="dfghj@mail.com", email3="fghjkk@mail.com", homepage="address.com",
-                      birthday="2", birthmonth="November", birthyear="1970",
-                      address2="one address", home="15", notes="some notes")
+    contact = Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
+    home_phone="", mobile="", work_phone="", fax="", email="", email2="", email3="", homepage="",
+    birthday="-", birthmonth="-", birthyear="", address2="", home="", notes="")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -7,14 +7,28 @@ def test_edit_contact_empty_lastname(app):
         if app.contact.lastname() > 0:
             app.contact.create(Contact(lastname=""))
     old_contacts = app.contact.get_contact_list()
-    app.contact.edit_first_contact(Contact(lastname="New name3"))
+    contact = Contact(lastname="loren")
+    contact.id = old_contacts[0].id
+    app.contact.edit_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 def test_edit_contact_birthday(app):
     if app.contact.count_contact() == 0:
         app.contact.create(Contact(lastname="Test name", birthday="-"))
     old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="someone", middlename="dita", lastname="loren", nickname="pool",
+                      title="research", company="cuberto", address="St. Petersburg",
+                      home_phone="8974567", mobile="789234567899", work_phone="234567889", fax="76834657",
+                      email="email@somemail.com",
+                      email2="dfghj@mail.com", email3="fghjkk@mail.com", homepage="address.com",
+                      birthday="2", birthmonth="November", birthyear="1970",
+                      address2="one address", home="15", notes="some notes")
+    contact.id = old_contacts[0].id
     app.contact.edit_first_contact(Contact(birthday="17"))
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
