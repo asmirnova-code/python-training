bccc4a329e043dc6a2b75ee8167f10996822bbde
Anastasia Smirnova <nastja.sm87@gmail.com>
2022-01-20 14:22:52 +0300

    Реализовано кеширование при загрузке списка групп

--- fixture/group.py
+++ fixture/group.py
@@ -19,6 +19,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -52,6 +53,7 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups()
+        self.group_cache = None
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
@@ -64,6 +66,7 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -73,6 +76,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups()
+        self.group_cache = None
 
     def delete_two_groups(self):
         wd = self.app.wd
@@ -84,6 +88,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups()
+        self.group_cache = None
 
     def return_to_groups(self):
         wd = self.app.wd
@@ -110,15 +115,19 @@ class GroupHelper:
         element = wd.find_element_by_name("group_header").get_attribute("textContent")
         return len(element)
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
+
 
 
 
