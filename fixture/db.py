709dee9dd2860809c800ff160c1ae84afa8be0ae
Anastasia Smirnova <nastja.sm87@gmail.com>
2022-02-11 13:14:43 +0300

    Добавлено условие отбора для загружаемых контактов

--- fixture/db.py
+++ fixture/db.py
@@ -31,7 +31,8 @@ class DbFixture():
         cursor = self.connection.cursor()
         try:
             cursor.execute("select id, firstname, middlename, lastname, nickname, company, title, address, home, mobile,"
-            " work, fax, email, email2, email3, homepage, bday, bmonth, byear, address2, phone2, notes from addressbook")
+            " work, fax, email, email2, email3, homepage, bday, bmonth, byear, address2, phone2, notes from addressbook "
+                           "where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, middlename, lastname, nickname, company, title, address, home, mobile,
             work, fax, email, email2, email3, homepage, bday, bmonth, byear, address2, phone2, notes) = row
