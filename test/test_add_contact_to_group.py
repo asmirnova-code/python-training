20fdae3d916ae919c7529df4fb0f8db85e0713c1
Anastasia Smirnova <nastja.sm87@gmail.com>
2022-02-14 18:14:07 +0300

    Добавление и удаление контакта из группы: внесены исправления в тесты

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -2,14 +2,14 @@ from model.contact import Contact
 from model.group import Group
 import random
 
-def test_add_contact_to_group(app, orm, db):
-    if len(db.get_contact_list()) == 0:
+def test_add_contact_to_group(app, orm):
+    if len(orm.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="TestName", lastname="Test"))
-    if len(db.get_group_list()) == 0:
+    if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="Test"))
-    contacts_list = db.get_contact_list()
+    contacts_list = orm.get_contact_list()
     contact = random.choice(contacts_list)
-    groups_list = db.get_group_list()
+    groups_list = orm.get_group_list()
     group = random.choice(groups_list)
     app.contact.assign_contact_by_id_to_group(contact.id, group.id)
     contacts_in_group = app.contact.get_contacts_in_group(group.id)
--- test/test_delete_contact_from_group.py
+++ test/test_delete_contact_from_group.py
@@ -2,16 +2,16 @@ from model.contact import Contact
 from model.group import Group
 import random
 
-def test_delete_contact_from_group(app, orm, db):
-    if len(db.get_group_list()) == 0:
+def test_delete_contact_from_group(app, orm):
+    if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="Test"))
-    if len(db.get_contact_list()) == 0:
+    if len(orm.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="TestName", lastname="Test"))
-    groups = db.get_group_list()
+    groups = orm.get_group_list()
     group = random.choice(groups)
     contacts_in_group = orm.get_contacts_in_group(group)
     if len(contacts_in_group) == 0:
-        contacts_list = db.get_contact_list()
+        contacts_list = orm.get_contact_list()
         random_contact = random.choice(contacts_list)
         app.contact.assign_contact_by_id_to_group(random_contact.id, group.id)
         contacts_in_group = orm.get_contacts_in_group(group)
